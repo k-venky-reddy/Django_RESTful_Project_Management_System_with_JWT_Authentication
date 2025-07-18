d75d1283076cf373439719f356f6f38cb4d978fe
K venkatesh Reddy <110481286+k-venky-reddy@users.noreply.github.com>
2025-07-18 10:07:34 +0530

    Delete project_management directory

--- project_management/project_management/wsgi.py
+++
@@ -1,5 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-# It is Used to Deployment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_management.settings')
-application = get_wsgi_application()
